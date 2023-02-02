b20dde0d5eab4f0a7ecfb27c3478f37cf484bae8
ZarryMyles <mahanth2709@gmail.com>
2023-02-02 22:29:44 +0530

    Text Panel changes

--- client/src/pages/podcastEditor/MainTopBar.jsx
+++ client/src/pages/podcastEditor/MainTopBar.jsx
@@ -1,17 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const MainTopBar = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        height: '74px',
-        width: '100%',
-        backgroundColor: '#7400B8',
-        position: "relative",
-        zIndex: 999999999
-    }}
-    >
+    <div className="flex relative z-[999999999] bg-[#7400B8] h-[73px] w-full">
       MainTopBar
     </div>
   );

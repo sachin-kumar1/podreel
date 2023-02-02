d3aa891afde708aacd8afcda094e973e0766256d
richinrix <richinvishal@gmail.com>
2023-02-02 23:40:19 +0530

    prim&secondary panel update

--- client/src/pages/podcastEditor/mainCanvas/components/SidePane/PrimaryPanel.js
+++ client/src/pages/podcastEditor/mainCanvas/components/SidePane/PrimaryPanel.js
@@ -65,15 +65,7 @@ const PannelItem = ({
       onClick={() => {
         updateSelectedPane(name);
       }}
-      className={`cursor-pointer flex flex-col items-center justify-center `}
-      style={{
-        // borderRight: isSlectedPane ? "4px solid #D9D9D9" : "0px",
-        marginTop,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
+      className="cursor-pointer flex flex-col items-center justify-center mb-8 "
     >
       <div
         className={`p-2`.concat(
@@ -81,9 +73,7 @@ const PannelItem = ({
         )}
       >
         <SVGToRender
-          className="text-[40px]"
-          // width={40}
-          // height={40}
+          className="text-[30px]"
           fill={isSlectedPane ? "black" : `white`}
         />
       </div>
@@ -94,26 +84,8 @@ const PannelItem = ({
 
 const PrimaryPanel = ({ selectedPane, updateSelectedPane }) => {
   return (
-    <div
-      className="px-2"
-      style={{
-        // width: "110px",
-        height: "100%",
-        backgroundColor: "#18191B",
-        borderRight: "2px solid white",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          width: "100%",
-        }}
-      >
+    <div className="px-2 pt-[73px] h-full border-r-2 bg-brandBlackBg1 flex justify-center items-start border-white overflow-auto">
+      <div className="flex flex-col w-full  items-center mb-[30px] pt-[20px] h-full ">
         {pannelContents.map((val, index) => {
           console.log("name is", val.SVG, val);
           const isSlectedPane = val.SVG === selectedPane;
